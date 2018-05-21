bc0ad18e5903c00cba52ac5de1ce3efeee84a932
Yuri <creator.and.dev@gmail.com>
2018-05-21 20:10:09 +0300

    Flappy Bird Demo

---
+++ core/src/info/fandroid/game/sprites/Tube.java
@@ -0,0 +1,54 @@
+package info.fandroid.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.security.PrivilegedExceptionAction;
+import java.util.Random;
+
+public class Tube {
+    //константы диапазон отклоенний труб по высоте/просветов
+    public static final int FLUCTUATION = 130;
+    public static final int TUBE_GAP = 100;
+    public static final int LOWEST_OPENING = 120;
+
+    public static final int TUBE_WIDTH = 52;
+
+    private Texture topTube, bottomTube;
+    private Vector2 posTopTube, posBotTube;
+
+    //позиции рандомные для труб
+    private Random rand;
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBotTube() {
+        return posBotTube;
+    }
+
+    public Tube(Float x) {
+        topTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
+
+        rand = new Random();
+
+        posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+
+    }
+
+    public void reposition(float x) {
+        posTopTube.set(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube.set(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+    }
+}
---
+++ core/src/info/fandroid/game/states/PlayState.java
@@ -0,0 +1,81 @@
+package info.fandroid.game.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.utils.Array;
+
+import info.fandroid.game.FlappyDemo;
+import info.fandroid.game.sprites.Bird;
+import info.fandroid.game.sprites.Tube;
+
+public class PlayState extends State {
+    public static final int TUBE_SPACING = 125;
+    public static final int TUBE_COUNT = 4;
+
+    private Bird bird;
+    private Texture bg;
+
+    private Array<Tube> tubes;
+
+
+    public PlayState(GameStateManager gsm) {
+        super(gsm);
+        bird = new Bird(50, 300);
+        camera.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+
+        bg = new Texture("bg.png");
+
+        //список труб
+        tubes = new Array<Tube>();
+
+        //создаем трубы через цикл
+        for (int i = 1; i < TUBE_COUNT; i++){
+            tubes.add(new Tube((float) (i * (TUBE_SPACING + Tube.TUBE_WIDTH))));
+        }
+    }
+
+    @Override
+    protected void handleInput() {
+        if (Gdx.input.justTouched()) {
+            bird.jump();
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInput();
+        bird.update(dt);
+
+        camera.position.x = bird.getPosition().x + 80;
+
+        for (Tube tube: tubes) {
+            if (camera.position.x - (camera.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()){
+                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+            }
+        }
+
+        camera.update();
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(camera.combined);
+        sb.begin();
+        sb.draw(bg, camera.position.x - (camera.viewportWidth / 2), 0);
+        sb.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
+
+        for (Tube tube: tubes) {
+            sb.draw(tube.getTopTube(), tube.getPosBotTube().x, tube.getPosTopTube().y);
+            sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
+        }
+
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
