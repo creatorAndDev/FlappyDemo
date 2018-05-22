728ca9edad941a45c7ae0a7c504b609159425f0e
Yuri <creator.and.dev@gmail.com>
2018-05-22 19:15:34 +0300

    + add земля и ее движение

--- core/src/info/fandroid/game/sprites/Tube.java
+++ core/src/info/fandroid/game/sprites/Tube.java
@@ -68,4 +68,9 @@ public class Tube {
     public boolean collides(Rectangle player) {
         return player.overlaps(boundsTop) || player.overlaps(boundsBot);
     }
+
+    public void dispose() {
+        topTube.dispose();
+        bottomTube.dispose();
+    }
 }
--- core/src/info/fandroid/game/states/MenuState.java
+++ core/src/info/fandroid/game/states/MenuState.java
@@ -13,6 +13,9 @@ public class MenuState extends State{
     public MenuState(GameStateManager gsm) {
         super(gsm);
 
+        //для того что бы меню на начальном экране не было мелким
+        camera.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+
         background = new Texture("bg.png");
         playBtn = new Texture("playbtn.png");
     }
@@ -31,9 +34,12 @@ public class MenuState extends State{
 
     @Override
     public void render(SpriteBatch sb) {
+        //для того что бы меню на начальном экране не было мелким
+        sb.setProjectionMatrix(camera.combined);
+
         sb.begin();
-        sb.draw(background, 0, 0, FlappyDemo.WIDTH, FlappyDemo.HEIGHT);
-        sb.draw(playBtn, (FlappyDemo.WIDTH / 2) - (playBtn.getWidth() / 2), FlappyDemo.HEIGHT / 2);
+        sb.draw(background, 0, 0);
+        sb.draw(playBtn, camera.position.x - playBtn.getWidth() / 2, camera.position.y);
         sb.end();
     }
 
@@ -41,5 +47,7 @@ public class MenuState extends State{
     public void dispose() {
         background.dispose();
         playBtn.dispose();
+
+        System.out.println("MenuState Dispose");
     }
 }
